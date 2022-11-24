9f708c43cdc601b9a40ff60d6b16d49f9ab1e802
Lucas Rodrigues <devlucas027@gmail.com>
2022-11-23 23:54:38 -0300

    Teste 'C:'

--- src/criptografia_python.py
+++ src/criptografia_python.py
@@ -1,7 +1,7 @@
 import hashlib
 import os
 
-diretorio = 'C:\Secret'
+diretorio ='C:\Users\Lucas Rodrigues\Documents\GitHub\Python\src\Secret'
 
 for files in os.listdir(diretorio):
     os.chdir(diretorio)
@@ -10,7 +10,7 @@ for files in os.listdir(diretorio):
         criptografar = hashlib.sha512(dados).hexdigest()
         novo_arquivo = '(criptogradado)' + os.path.basename(files)
         with open(novo_arquivo, 'wb') as novo:
-            novo.write(criptografar*0xFF)
+            novo.write(criptografar * 0xFF)
             novo.close()
             rb.close()
 
